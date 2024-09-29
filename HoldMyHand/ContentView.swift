04dc9a6e5e2ce25e5ac9cbc6c24fa8080cb266d2
seongahshin <cindy903c@naver.com>
2024-09-29 22:35:30 +0900

    Initial Commit

---
+++ HoldMyHand/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HoldMyHand
+//
+//  Created by 신승아 on 9/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HoldMyHand/HoldMyHandApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoldMyHandApp.swift
+//  HoldMyHand
+//
+//  Created by 신승아 on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HoldMyHandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
