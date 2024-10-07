ecbe07a46a05f6240d431d1dcef867b88c6360aa
bbang2001 <113842420+bbang2001@users.noreply.github.com>
2024-10-07 12:12:15 +0900

    feat: 헬롱

--- HoldMyHand/ContentView.swift
+++ HoldMyHand/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            
+            Text("헬롱")
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
